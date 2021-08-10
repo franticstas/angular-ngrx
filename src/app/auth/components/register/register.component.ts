a236097c049341cb6e4ea2d6d484d2c85bcc0b00
stas <franticstas@gmail.com>
2021-08-10 07:42:07 +0300

    add selector]

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
-import {Store} from '@ngrx/store'
+import {select, Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
 
 import {registerAction} from '../../store/actions/register.action'
+import {isSubmittingSelector} from '../../store/selectors'
 
 @Component({
   selector: 'mc-register',
@@ -10,12 +12,18 @@ import {registerAction} from '../../store/actions/register.action'
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  form!: FormGroup
+  form: FormGroup | any
+  isSubmitting$: Observable<boolean> | any
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
+    this.initializeValues()
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
   }
 
   initializeForm(): void {
